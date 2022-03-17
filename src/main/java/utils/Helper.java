9d474e55b3d90599b14bd6ee36b6f490b176afc3
Anchit Bhatia <anchitbhatia23@gmail.com>
2022-03-17 03:00:35 -0700

    Added Helper class

---
+++ src/main/java/utils/Helper.java
@@ -0,0 +1,33 @@
+package utils;
+
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.lang.reflect.Type;
+
+public class Helper {
+
+    public static ApplicationConfig parseArgs(String[] args) throws Exception {
+        if (args.length != 4 || !args[0].equals(Constants.TYPE_FLAG) || !args[2].equals(Constants.CONFIG_FLAG)) {
+            throw new Exception("INVALID ARGS");
+        }
+        String type = args[Constants.TYPE_INDEX];
+        String file = args[Constants.FILE_INDEX];
+
+        Type classType;
+        switch (type) {
+            case Constants.TYPE_PRODUCER -> {
+                classType = ProducerConfig.class;
+            }
+            case Constants.TYPE_BROKER -> {
+                classType = BrokerConfig.class;
+            }
+            default -> throw new Exception("INVALID TYPE");
+        }
+        Gson gson = new Gson();
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        Object config = gson.fromJson(reader, classType);
+        return new ApplicationConfig(type, config);
+    }
+}
