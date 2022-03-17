daefd5ec2ec028f1a2d0689b62f35412b61f36e1
Anchit Bhatia <anchitbhatia23@gmail.com>
2022-03-17 03:00:57 -0700

    Added constants class

---
+++ src/main/java/utils/Constants.java
@@ -0,0 +1,15 @@
+package utils;
+
+public class Constants {
+    public static final String TYPE_FLAG = "-type";
+    public static final int TYPE_INDEX = 1;
+    public static final String CONFIG_FLAG = "-config";
+    public static final int FILE_INDEX = 3;
+
+    public static final String TYPE_PRODUCER = "producer";
+    public static final String TYPE_BROKER = "broker";
+    public static final String TYPE_CONSUMER = "consumer";
+    public static final String TYPE_NULL = null;
+
+    public static final int MAX_BYTES = 1000;
+}
