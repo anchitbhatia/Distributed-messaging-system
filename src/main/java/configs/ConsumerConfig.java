177e6dbe144c71aec6d6ccbec9e0902818222e16
Anchit Bhatia <anchitbhatia23@gmail.com>
2022-03-18 01:47:48 -0700

    Added consumerconfig class

---
+++ src/main/java/configs/ConsumerConfig.java
@@ -0,0 +1,36 @@
+package configs;
+
+import utils.Node;
+
+public class ConsumerConfig {
+    private Node host;
+    private Node broker;
+    private String topic;
+    private String file;
+    private long startPosition;
+    private long timeout;
+
+    public Node getHost() {
+        return host;
+    }
+
+    public Node getBroker() {
+        return broker;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public Long getStartPosition() {
+        return startPosition;
+    }
+
+    public String getFile() {
+        return file;
+    }
+
+    public long getTimeout() {
+        return timeout;
+    }
+}
