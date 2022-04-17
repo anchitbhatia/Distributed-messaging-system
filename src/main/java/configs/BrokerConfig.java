740b265a2749461988691e6853c99e2c3f5c4afe
Anchit Bhatia <anchitbhatia23@gmail.com>
2022-04-17 03:19:37 -0700

    modified broker config to accomodate diffferent type of brokers

--- src/main/java/configs/BrokerConfig.java
+++ src/main/java/configs/BrokerConfig.java
@@ -7,10 +7,20 @@ import utils.Node;
  * @author anchitbhatia
  */
 public class BrokerConfig {
+    private boolean isLeader;
     private Node host;
+    private Node leader;
+
+    public boolean isLeader() {
+        return isLeader;
+    }
 
     public Node getHost() {
         return host;
+    }
+
+    public Node getLeader() {
+        return leader;
     }
 
 }
