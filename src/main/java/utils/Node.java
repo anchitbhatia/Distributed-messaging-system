dc997e57287e3c03f2fde472daea3d1f4eaead50
Anchit Bhatia <anchitbhatia23@gmail.com>
2022-04-17 03:19:06 -0700

    Added id in node

--- src/main/java/utils/Node.java
+++ src/main/java/utils/Node.java
@@ -7,10 +7,21 @@ package utils;
 public class Node{
     private final String hostName;
     private final int port;
+    private int id;
 
     public Node(String hostName, int port) {
         this.hostName = hostName;
         this.port = port;
+    }
+
+    public Node(String hostName, int port, int id) {
+        this.hostName = hostName;
+        this.port = port;
+        this.id = id;
+    }
+
+    public void setId(int id){
+        this.id = id;
     }
 
     public String getHostName() {
@@ -21,8 +32,12 @@ public class Node{
         return port;
     }
 
+    public int getId() {
+        return id;
+    }
+
     @Override
     public String toString() {
-        return hostName + ":" + port;
+        return id + ":" + hostName + ":" + port;
     }
 }
